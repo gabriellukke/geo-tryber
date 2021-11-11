32456f894551e9dc52fa1b1d29ea2a61b0953f36
gabriellukke <gabriellukke@outlook.com>
2021-11-11 13:38:24 -0300

    feat: countrydetails

---
+++ src/pages/CountryDetails.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class CountryDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      country: {}
+    }
+  }
+
+  render() {
+    return (
+      <section>
+        
+      </section>
+    )
+  }
+}
