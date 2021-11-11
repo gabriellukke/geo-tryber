7f0c1d46a6ac4cd8a221de64bef31b189ec1ec6b
gabriellukke <gabriellukke@outlook.com>
2021-11-11 10:52:38 -0300

    refact: Home page states passed to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Search from './pages/Search';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      allCountries: [],
+    };
+  }
+
+  fetchCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((allCountries) => this.setState({ loading: false, allCountries }));
+  };
+
   render() {
     return (
       <>
-        <h1 className="App">
-          GEOTRYBER
-        </h1>
-        <Search />
+        <h1 className="App">GEOTRYBER</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <Home {...props} fetchCountries={this.fetchCountries} countries={this.state.allCountries} />
+              )}
+            />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </BrowserRouter>
       </>
     );
   }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,8 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      loading: false,
-      allCountries: [],
-    }
-  }
-
   componentDidMount() {
-    fetch('https://restcountries.com/v3.1/all')
-      .then((data) => data.json())
-      .then((allCountries) => this.setState({ allCountries }))
+    this.props.fetchCountries()
   }
 
   render() {
