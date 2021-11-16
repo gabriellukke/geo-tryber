448e339bc1e7027b62a5490ea1cea3ec01e9fb46
gabriellukke <gabriellukke@outlook.com>
2021-11-16 13:30:16 -0300

    Filtragem de dados na page countryDetails

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -1,19 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class CountryDetails extends Component {
   constructor() {
     super();
 
     this.state = {
-      country: {}
+      countries: [],
+      country: {},
+    };
+
+    this.findCountry = this.findCountry.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((response) => response.json())
+      .then((countries) => this.setState({ countries }));
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    const { countries } = this.state;
+    console.log('update', prevState);
+    if (prevState.countries.length !== countries.length) {
+      console.log('entrou no if');
+      this.findCountry();
     }
   }
 
+  findCountry() {
+    //const { countries } = this.state;
+    const { match } = this.props;
+    const { countries } = this.state;
+    console.log(countries);
+    const country = countries.find(
+      (country) => country.cca3 === match.params.code
+    );
+    console.log(country);
+    this.setState({ country });
+  }
+
   render() {
-    return (
-      <section>
-        Details
-      </section>
-    )
+    console.log('Atualizado');
+    const { country } = this.state;
+    console.log('Filtrado', country);
+
+    return <section>{country.name && <h1>{country.name.common}</h1>}</section>;
   }
 }
