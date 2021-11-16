434f9dbd4231ef3e794a5b7e5ab35c6baa3cae5b
gabriellukke <gabriellukke@outlook.com>
2021-11-16 13:29:16 -0300

    Criação de rotas e do método fetchCountries

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
+
 import './App.css';
+
 import Home from './pages/Home';
 import Search from './pages/Search';
+import CountryDetails from './pages/CountryDetails';
 
 class App extends React.Component {
   constructor() {
@@ -12,12 +15,35 @@ class App extends React.Component {
       loading: false,
       allCountries: [],
     };
+
+    this.fetchCountries = this.fetchCountries.bind(this);
+  }
+
+  fetchCountries() {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((response) => response.json())
+      .then((allCountries) => this.setState({ allCountries }))
   }
 
   render() {
+    const { allCountries } = this.state;
     return (
       <>
         <h1>GEOTRYBER</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={ (props) => <Home { ...props } fetchApi={ this.fetchCountries } allCountries={ allCountries } /> }
+            />
+            <Route path="/search" component={ Search } />
+            <Route
+              path="/country/:code"
+              render={ (props) => <CountryDetails { ...props } countries={ allCountries } fetchApi={ this.fetchCountries } /> }
+            />
+          </Switch>
+        </BrowserRouter>
       </>
     );
   }
